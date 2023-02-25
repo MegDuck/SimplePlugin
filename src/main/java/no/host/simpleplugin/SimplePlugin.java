b6397297925718fb9dedd05d9871556e5ca46cd7
MegDuck <65822271+MegDuck@users.noreply.github.com>
2023-02-25 18:00:13 +0800

    Initial commit

---
+++ src/main/java/no/host/simpleplugin/SimplePlugin.java
@@ -0,0 +1,17 @@
+package no.host.simpleplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimplePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
